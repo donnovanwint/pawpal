794e6ac278f3976563a31d0ed5057f1af888c246
Suleman Khan <sulemankk010@gmail.com>
2024-01-11 19:51:15 +0500

    DTO

--- backend/src/pet/dto/create-pet.dto.ts
+++ backend/src/pet/dto/create-pet.dto.ts
@@ -1,1 +1,37 @@
-export class CreatePetDto {}
+import { IsNotEmpty, IsString, MaxLength, IsOptional, IsInt, IsEnum } from "class-validator";
+
+export enum AnimalType {
+    CAT = 'cat',
+    DOG = 'dog',
+}
+
+export enum Gender {
+    MALE = 'male',
+    FEMALE = 'female',
+    OTHER = 'other'
+}
+
+export class CreatePetDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(255)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    gender?: Gender;
+
+    @IsOptional()
+    @IsInt()
+    age?: number;
+
+    @IsOptional()
+    @IsEnum(AnimalType)
+    animalType?: AnimalType;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(1000)
+    description?: string;
+}
